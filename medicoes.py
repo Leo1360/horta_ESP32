9b9280eca7dd23cad56fe7bca7d3fe9404f8a0ac
Leonardo Henrique Silva <leonardosilva1360@gmail.com>
2023-09-17 18:57:03 -0300

    add sensor capacitivo de umidade de solo

--- medicoes.py
+++ medicoes.py
@@ -1,5 +1,6 @@
 import json
-import machine
+from machine import Pin, ADC
+from time import sleep
 from easydate import DateTime
 
 sensores = []
@@ -33,6 +34,30 @@ def getLeituras():
             medicoes.append({"nome":sensor["nome"]})
     return {"dataHora":hora,"medicoes":medicoes}
 
+def filtroDesvPad(arr, num_std_dev=1):
+    if len(arr) == 0:
+        return 0
+
+    # Calculate the mean of the input array
+    arr_mean = sum(arr) / len(arr)
+
+    # Calculate the standard deviation of the input array
+    arr_std = (sum((x - arr_mean) ** 2 for x in arr) / len(arr)) ** 0.5
+
+    # Calculate the threshold for filtering
+    threshold = arr_std * num_std_dev
+
+    # Filter out values more than 'num_std_dev' standard deviations away from the mean
+    filtered_arr = [x for x in arr if abs(x - arr_mean) <= threshold]
+
+    return filtered_arr
+
+def mediaArray(arr):
+    if len(arr) == 0:
+        return 0  # Avoid division by zero if all values are filtered out
+    media = sum(arr) / len(arr)
+    return media
+
 def rdht11(cfg):
     from dht import DHT11
     d = DHT11(machine.Pin(cfg["pinos"][0])) 
@@ -45,15 +70,34 @@ def rdht11(cfg):
 
 def rdht22(cfg):
     from dht import DHT22
-    d = DHT22(machine.Pin(cfg["pinos"][0]))
+    d = DHT22(Pin(cfg["pinos"][0]))
     d.measure()
     return {
-        "tmp":d.temperature(),
-        "hum":d.humidity(),
+        "temperatura":d.temperature(),
+        "umidade":d.humidity(),
         "nome":cfg["nome"]
     }
 
+def capacitivoUmidadeSolo(cfg):
+    p = cfg["pinos"][0]
+    cap = ADC(Pin(p))
+    cap.atten(ADC.ATTN_11DB)
+
+    amostras = []
+    for c in range(0,20):
+        amostras.append(cap.read())
+        sleep(0.01)
+    amostras = filtroDesvPad(amostras,1)
+    umidade = 100 - ((mediaArray(amostras)-1024) * 0.05537)
+
+    return {
+        "umidade":umidade,
+        "nome":cfg["nome"]
+    }
+
+
 sensorFunctions = {
     "DHT11":rdht11,
-    "DHT22":rdht22
+    "DHT22":rdht22,
+    "HumidadeSolo":capacitivoUmidadeSolo
 }
